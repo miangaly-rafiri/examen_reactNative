5f8eb4721d74d688ed47c922a14eadf01f5d6efb
Miangaly Rafiringa <miangalyrafi@gmail.com>
2025-11-03 00:12:17 +0100

    J'ai remis l'url de l'API en localhost au lieu de mon IP

--- my-book-app/services/api.ts
+++ my-book-app/services/api.ts
@@ -1,7 +1,7 @@
 import { Book, BookFormData } from '../types/book';
 
-// const API_URL = 'http://localhost:3000';
-const API_URL = 'http://192.168.1.69:3000';
+const API_URL = 'http://localhost:3000';
+// const API_URL = 'http://192.168.1.69:3000';
 
 
 export const api = {
